084a033d799529a7e15c3fd6f3f271885fa5d8e8
puresyntax71 <34715246+puresyntax71@users.noreply.github.com>
2025-03-01 06:06:34 +0800

    Uninstall page throwing an error (#519)
    * Add basic test for uninstall page.
    
    * Update decorating service.
    
    * Revert "Update decorating service."
    
    This reverts commit b12aafef352e3ce3168cc1253bf330f270a2efd9.
    
    * Fix decoration.
    
    * Minor update.
    
    * Overwrite service instead of decorate.
    
    * Minor update.

--- src/ModuleInstaller.php
+++ src/ModuleInstaller.php
@@ -2,34 +2,12 @@
 
 namespace Drupal\civicrm_entity;
 
-use Drupal\Core\DrupalKernelInterface;
-use Drupal\Core\Extension\ModuleHandlerInterface;
 use Drupal\Core\Extension\ModuleInstaller as ExtensionModuleInstaller;
-use Drupal\Core\Extension\ModuleInstallerInterface;
-use Drupal\Core\Database\Connection;
-use Drupal\Core\Logger\LoggerChannelFactoryInterface;
-use Drupal\Core\Update\UpdateHookRegistry;
 
 /**
- * Class ContentUninstallValidator.
+ * Class for ModuleInstaller.
  */
 class ModuleInstaller extends ExtensionModuleInstaller {
-
-  /**
-   * The module installer.
-   *
-   * @var \Drupal\Core\Extension\ModuleInstallerInterface
-   */
-  protected $moduleInstaller;
-
-  /**
-   * {@inheritdoc}
-   */
-  public function __construct(ModuleInstallerInterface $module_installer, $root, ModuleHandlerInterface $module_handler, DrupalKernelInterface $kernel, Connection $connection, UpdateHookRegistry $update_registry, LoggerChannelFactoryInterface $logger_factory) {
-    $logger = $logger_factory->get('civicrm_entity');
-    parent::__construct($root, $module_handler, $kernel, $connection, $update_registry, $logger);
-    $this->moduleInstaller = $module_installer;
-  }
 
   /**
    * {@inheritdoc}
